aba09c310e661a9a5e3ac10fee2a2b8ee3d1b600
YONGSHENGHUA0130 <128607433+YONGSHENGHUA0130@users.noreply.github.com>
2023-06-24 02:31:21 +0800

    done

--- user login and register page/user login and register/index.php
+++ user login and register page/user login and register/index.php
@@ -1,42 +1,24 @@
-
-<!DOCTYPE html>
-<html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>User login and register</title>
-  <link rel="stylesheet" href="./userlogin.css">
-
-</head>
-<body>
-
-<a href="logout.php">Logout</a>
-
-<br>
-Hello, Username
-<div class="login-page">
-  <div class="form">
-    <form class="register-form">
-      <input type="text" placeholder="Name"/>
-      <input type="password" placeholder="Password"/>
-      <input type="text" placeholder="Email Address"/>
-      <button>register</button>
-      <p class="message">Already registered ? <a href="#">Sign In</a></p>
-    </form>
-    <form class="login-form">
-      <input type="text" placeholder="Name"/>
-      <input type="password" placeholder="Password"/>
-      <button>Login</button>
-      <p class="message">Not registered ? <a href="#">Create an account</a></p>
-    </form>
-  </div>
-</div>
-
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
-
-</body>
-</html>
-
 <?php 
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+	$user_data = check_login($con);
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My website</title>
+</head>
+<body>
+
+	<a href="logout.php">Logout</a>
+	<h1>This is the index page</h1>
+
+	<br>
+	Hello, <?php echo $user_data['user_name']; ?>
+</body>
+</html>
